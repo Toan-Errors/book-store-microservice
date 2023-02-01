5dc66831b02009c41c20d399a0b8c19d3dd32bb4
ToànErrors <72167690+Toan-Errors@users.noreply.github.com>
2023-02-01 22:38:53 +0700

    update

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,6 +1,7 @@
 export * from './common.module';
 export * from './common.service';
 export * from './book/create-book.dto';
+export * from './book/list-all-entities.dto';
 export * from './user/create-user.dto';
 export * from './user/login-user.dto';
 
